76c03f2002eb9c8b728f175082eb54c26e3ef076
Nick Kinnan <nick@nickkinnan.com>
2017-10-23 15:21:31 -0700

    rev version for new nuget release

--- makenuget.cmd
+++ makenuget.cmd
@@ -1,2 +1,9 @@
+rem =======================================================
+Did you remember to:
+1) build both debug and release
+2) update the nuspec to the new revision #
+3) update the dll properties to the new revision #
+rem =======================================================
+pause
 nuget pack Package.nuspec
 pause
